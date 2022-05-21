12fd189a90b82ba472da78cca8a3a69425e4c348
Claudio <claudiojlf@gmail.com>
2022-05-20 23:31:04 -0300

    added main logic function

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import * as C from './App.styles'
 import Interest from './components/Interest';
 
+
 function App() {
   let typedValue:string = '12'
+  
+  const convertInterest = (interest:number, time:string , toTime:string) => {
+    type ObjectKey = keyof typeof periods;
+    const periods = {
+      Week: 52,
+      Month: 12,
+      Year: 1
+    }
+    const periodGiven = time as ObjectKey
+    const periodToConvert = toTime as ObjectKey
+    
+    const expoent = (periods[periodGiven] / periods[periodToConvert]) * Math.log(1 + interest/100)
+    
+    const convertedInterest = Math.E ** (expoent)
+    const formatedInterest = ((convertedInterest - 1) * 100).toFixed(4)
+
+    return formatedInterest
+  }
 
   return (
   <>
